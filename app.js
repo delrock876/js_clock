ac56bd3f9b070482fedd7ac594bf16cd11f756be
Sabrina Rocha <delrock876@gmail.com>
2020-03-08 14:57:20 -0700

    clock conversion

---
+++ app.js
@@ -0,0 +1,23 @@
+// const today = new Date()
+// let meridian = `AM`
+// let minutes = today.getMinutes()
+// let current = `${today.getHours()}:${minutes + meridian}`
+
+// //displays am or pm
+// if (today.getHours() > 12) {
+//   meridian = `PM`
+// }
+
+// const convertTime = _ => {
+//   if (minutes < 10) {
+//     minutes = `0${minutes}`
+//   }
+//   //converts from military time
+//   if (today.getHours() > 12) {
+//     return current = `${today.getHours() - 12}:${minutes + meridian}`
+//   }
+// }
+
+// console.log(convertTime())
+
+// let time = document.getElementById('currentTime').innerHTML = convertTime()
